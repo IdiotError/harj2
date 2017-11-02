a1c7e958e5b98f820e4276fc4ee6833cd5c11174
Unknown <tuoppi922@hotmail.com>
2017-11-02 13:10:45 +0200

    bootstrap, default controller

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>CodeIgniter esimerkki</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
     <ul>
@@ -10,5 +11,5 @@
       <li><a href="<?php echo site_url('test/kolmas');?>">test/kolmas</a></li>
       <li><a href="<?php echo site_url('test/neljas');?>">test/neljäs</a></li>
       <li><a href="<?php echo site_url('oppilas/nayta_oppilaat');?>">Opiskelijat</a></li><!--site urliin ei tiedostonimi vaan class ja controllerissa oleva funktion nimi-->
-
     </ul>
+<div class="container">
--- application/views/oppilas/nayta_oppilaat.php
+++ application/views/oppilas/nayta_oppilaat.php
@@ -1,5 +1,5 @@
 <h1>opiskelijat</h1>
-<table border="1">
+<table class="table table-bordered table-striped">
   <tr>
     <th>etunimi</th><th>sukunimi</th><th>syntymävuosi</th><th>poista</th>
   </tr>
@@ -9,7 +9,7 @@
      echo '<td>'.$r['etunimi'].'</td>';
      echo '<td>'.$r['sukunimi'].'</td>';
      echo '<td>'.$r['syntymavuosi'].'</td>';
-     echo '<td><a href="'.site_url('oppilas/poista_oppilas').'/'.$r['idOpiskelija'].'">Exterminate</a></td>';
+     echo '<td><button class="btn btn-danger"><a href="'.site_url('oppilas/poista_oppilas').'/'.$r['idOpiskelija'].'">Exterminate</a></button></td>';
      echo '</tr>';
    }
    ?>
